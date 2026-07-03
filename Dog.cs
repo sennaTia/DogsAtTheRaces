2b80ed6d5d0eb81309968c80e2555a400300c5a2
sennaTia <1214998@student.roc-nijmegen.nl>
2026-07-03 12:52:29 +0200

    Made dog good

--- Dog.cs
+++ Dog.cs
@@ -8,5 +8,26 @@ namespace DogsAtTheRaces
 {
     internal class Dog
     {
+        public PictureBox MyPictureBox;
+        public int Location;
+        public int StartingPosition;
+        public int RaceTrackLength;
+
+        public static Random Randomizer = new Random();
+
+        public bool Run()
+        {
+            Location += Randomizer.Next(1, 5);
+
+            MyPictureBox.Left = StartingPosition + Location;
+
+            return (MyPictureBox.Left >= RaceTrackLength);
+        }
+
+        public void TakeStartingPosition()
+        {
+            MyPictureBox.Left = StartingPosition;
+            Location = 0;
+        }
     }
 }
